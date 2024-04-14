3a9f8f07fbca785740f5266910086d2ce7dafdc3
Yujiro-13 <yujiro.sss.2022@outlook.jp>
2024-04-15 01:16:26 +0900

    フォント変更 posh-git追加

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,3 +10,18 @@ $psdir="C:\Users\81807\Documents\PowerShell\autoload"
 # ディレクトリ内のすべてのps1ファイルを読み込む
 Get-ChildItem "${psdir}\*.ps1" | %{.$_}
 
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/paradox.omp.json" | Invoke-Expression
+
+Import-module posh-git
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
